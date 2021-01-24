7fe8da0f7697efec8e90562d8a2a05b35a633f86
shane <shanecolbyzero8@gmail.com>
2021-01-24 12:21:34 -0600

    show index

--- app/controllers/api/candles_controller.rb
+++ app/controllers/api/candles_controller.rb
@@ -3,4 +3,10 @@ class Api::CandlesController < ApplicationController
     @candles = Candle.all
     render 'index.json.jb'
   end
+
+  def show
+    @candle = Candle.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/candles" => "candles#index"
+    get '/candles/:id' => 'candles#show'
+
   end
 end
